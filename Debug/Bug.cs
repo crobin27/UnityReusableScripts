84e9e157e9a6f9061aa23d2cf7e270a544cce75a
crobin27 <crobin27@calpoly.edu>
2024-05-23 08:33:25 -0700

    added Bug.cs

--- Debug/Bug.cs
+++ Debug/Bug.cs
@@ -1,44 +1,52 @@
 ﻿using UnityEngine;
 using System;
 
-public static class Bug
+namespace RobinsonGaming.Debugging
 {
-    public static event Action<Exception> OnException;
-
-    // Toggle this to turn Bugging on/off.
-    private static bool _isBugMode = false;
-
-    public static void Log(string message)
+    /// <summary>
+    /// The Bug class provides enhanced control over debugging logs in Unity.
+    /// It allows toggling debugging logs on or off and provides methods for logging messages, warnings, errors, and exceptions.
+    /// </summary>
+    public static class Bug
     {
-        if (_isBugMode)
+        // Event for handling exceptions
+        public static event Action<Exception> OnException;
+
+        // Toggle this to turn Bugging on/off.
+        private static bool _isBugMode = false;
+
+        public static void Log(string message)
         {
-            Debug.Log("Bug: " + message);
+            if (_isBugMode)
+            {
+                Debug.Log("Bug: " + message);
+            }
         }
-    }
 
-    public static void LogError(string message = "")
-    {
-        if (_isBugMode)
+        public static void LogError(string message = "")
         {
-            Debug.LogError("Bug Error: " + message);
+            if (_isBugMode)
+            {
+                Debug.LogError("Bug Error: " + message);
+            }
         }
-    }
 
-    public static void LogException(Exception ex)
-    {
-        OnException?.Invoke(ex);
-        if (_isBugMode)
+        public static void LogException(Exception ex)
         {
-            Debug.LogException(ex);
-            Log(ex.ToString());
+            OnException?.Invoke(ex);
+            if (_isBugMode)
+            {
+                Debug.LogException(ex);
+                Log(ex.ToString());
+            }
         }
-    }
 
-    public static void LogWarning(string message)
-    {
-        if (_isBugMode)
+        public static void LogWarning(string message)
         {
-            Debug.LogWarning("Bug Warning: " + message);
+            if (_isBugMode)
+            {
+                Debug.LogWarning("Bug Warning: " + message);
+            }
         }
     }
 }
